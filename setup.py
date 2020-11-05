69bafdec0bb891a4cfd27cd804ba21aa2ec45ebf
Sébastien Weber <seba.weber@gmail.com>
2020-11-05 21:58:29 +0100

    update plugin_info

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-import distutils.dir_util
-from distutils.command import build
-import os, sys, re
+from plugin_info import SHORT_PLUGIN_NAME, packages_required, package_url, author_email, author, description
+PLUGIN_NAME = f'pymodaq_plugins_{SHORT_PLUGIN_NAME}'
+
+import importlib
+import sys
 try:
     import setuptools
     from setuptools import setup, find_packages
@@ -10,20 +12,20 @@ except ImportError:
     from distutils.core import setup
     from distutils.command import install
 
-from pymodaq_plugins.version import get_version
+version = importlib.import_module('.version', PLUGIN_NAME)
 
 with open('README.rst') as fd:
     long_description = fd.read()
 
 setupOpts = dict(
-    name='pymodaq_plugins',
-    description='Hardware plugins for PyMoDAQ',
+    name=PLUGIN_NAME,
+    description=description,
     long_description=long_description,
-    license='MIT',
-    url='http://pymodaq.cnrs.fr',
-    author='Sébastien Weber',
-    author_email='sebastien.weber@cemes.fr',
-    classifiers = [
+    license='CECILL B',
+    url=package_url,
+    author=author,
+    author_email=author_email,
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Other Environment",
@@ -34,58 +36,17 @@ setupOpts = dict(
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: User Interfaces",
-        ],)
+    ], )
 
-def listAllPackages(pkgroot):
-    path = os.getcwd()
-    n = len(path.split(os.path.sep))
-    subdirs = [i[0].split(os.path.sep)[n:] for i in os.walk(os.path.join(path, pkgroot)) if '__init__.py' in i[2]]
-    return ['.'.join(p) for p in subdirs]
-
-
-allPackages = (listAllPackages(pkgroot='pymodaq_plugins')) #+
-               #['pyqtgraph.'+x for x in helpers.listAllPackages(pkgroot='examples')])
-
-
-
-def get_packages():
-
-    packages=find_packages()
-    for pkg in packages:
-        if 'hardware.' in pkg:
-            packages.pop(packages.index(pkg))
-    return packages
-
-allPackages = get_packages()
-
-hardware_path=os.path.join('pymodaq_plugins','hardware')
-
-def get_hardware_packages(base_dir='',packages={}):
-    for pack in os.scandir(base_dir):
-        if pack.is_dir():
-            if '__pycache__' != pack.name:
-                packages.update({os.path.relpath(pack.path,'pymodaq_plugins'): '*.*'})
-                get_hardware_packages(pack.path,packages)
-    return packages
-
-packages_data=dict([])
-get_hardware_packages(hardware_path,packages_data)
 
 setup(
-    version=get_version(),
-     #cmdclass={'build': Build,},
-    #           'install': Install,
-    #           'deb': helpers.DebCommand,
-    #           'test': helpers.TestCommand,
-    #           'debug': helpers.DebugCommand,
-    #           'mergetest': helpers.MergeTestCommand,
-    #           'style': helpers.StyleCommand},
+    version=version.get_version(),
     packages=find_packages(),
     package_data={'': ['*.dll']},
-    entry_points={'pymodaq.plugins': 'default = pymodaq_plugins'},
+    entry_points={'pymodaq.plugins': f'default = {PLUGIN_NAME}'},
     install_requires=[
         'pymodaq>=2.0',
-        ],
+        ]+packages_required,
     **setupOpts
 )
 
