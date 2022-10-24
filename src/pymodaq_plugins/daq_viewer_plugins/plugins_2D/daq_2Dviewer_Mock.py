8aacdabbafc56d9e8a806cdf03faa134d26624c4
Sébastien Weber <seba.weber@gmail.com>
2022-10-25 00:02:02 +0200

    Update daq_2Dviewer_Mock.py

--- src/pymodaq_plugins/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Mock.py
+++ src/pymodaq_plugins/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Mock.py
@@ -1,7 +1,7 @@
 from qtpy.QtCore import QThread, Slot, QRectF
 from qtpy import QtWidgets
 import numpy as np
-import pymodaq.daq_utils.math_utils as mylib
+import pymodaq.daq_utils.math_utils as mutils
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, main, comon_parameters
 from easydict import EasyDict as edict
 from collections import OrderedDict
@@ -9,14 +9,21 @@ from pymodaq.daq_utils.daq_utils import ThreadCommand, getLineInfo, DataFromPlug
 from pymodaq.daq_utils.array_manipulation import crop_array_to_axis, crop_vector_to_axis
 
 
-
 class DAQ_2DViewer_Mock(DAQ_Viewer_base):
     """
+        =============== ==================
+        **Attributes**   **Type**
+        *params*         dictionnary list
+        *x_axis*         1D numpy array
+        *y_axis*         1D numpy array
+        =============== ==================
 
+        See Also
+        --------
+        utility_classes.DAQ_Viewer_base
     """
 
     params = comon_parameters + [
-        {'title': 'Wait time (ms)', 'name': 'wait_time', 'type': 'int', 'value': 100, 'default': 100, 'min': 0},
         {'title': 'Nimages colors:', 'name': 'Nimagescolor', 'type': 'int', 'value': 1, 'default': 1, 'min': 0,
          'max': 3},
         {'title': 'Nimages pannels:', 'name': 'Nimagespannel', 'type': 'int', 'value': 1, 'default': 0, 'min': 0},
@@ -25,13 +32,12 @@ class DAQ_2DViewer_Mock(DAQ_Viewer_base):
         {'title': 'rolling', 'name': 'rolling', 'type': 'int', 'value': 1, 'min': 0},
         {'title': 'Nx', 'name': 'Nx', 'type': 'int', 'value': 100, 'default': 100, 'min': 1},
         {'title': 'Ny', 'name': 'Ny', 'type': 'int', 'value': 200, 'default': 200, 'min': 1},
-        {'title': 'Amp', 'name': 'amp', 'type': 'int', 'value': 20, 'default': 20, 'min': 1},
+        {'title': 'Amp', 'name': 'Amp', 'type': 'int', 'value': 20, 'default': 20, 'min': 1},
         {'title': 'x0', 'name': 'x0', 'type': 'slide', 'value': 50, 'default': 50, 'min': 0},
         {'title': 'y0', 'name': 'y0', 'type': 'float', 'value': 100, 'default': 100, 'min': 0},
         {'title': 'dx', 'name': 'dx', 'type': 'float', 'value': 20, 'default': 20, 'min': 1},
         {'title': 'dy', 'name': 'dy', 'type': 'float', 'value': 40, 'default': 40, 'min': 1},
         {'title': 'n', 'name': 'n', 'type': 'int', 'value': 1, 'default': 1, 'min': 1},
-        {'title': 'Angle:', 'name': 'angle', 'type': 'float', 'value': 0, 'default': 0},
         {'title': 'amp_noise', 'name': 'amp_noise', 'type': 'float', 'value': 4, 'default': 0.1, 'min': 0},
 
         {'title': 'Cam. Prop.:', 'name': 'cam_settings', 'type': 'group', 'children': []},
@@ -44,8 +50,8 @@ class DAQ_2DViewer_Mock(DAQ_Viewer_base):
         self.y_axis = None
         self.live = False
         self.ind_commit = 0
+        self.ind_data = 0
         self._ROI = dict(position=[10, 10], size=[5, 5])
-
 
     @Slot(QRectF)
     def ROISelect(self, roi_pos_size: QRectF):
@@ -66,23 +72,39 @@ class DAQ_2DViewer_Mock(DAQ_Viewer_base):
             --------
             set_Mock_data
         """
-        self.base_Mock_data()
+        self.set_Mock_data()
 
-    def base_Mock_data(self):
+    def set_Mock_data(self):
+        """
+            | Set the x_axis and y_axis with a linspace distribution from settings parameters.
+            |
+
+            Once done, set the data mock with parameters :
+                * **Amp** : The amplitude
+                * **x0** : the origin of x
+                * **dx** : the derivative x pos
+                * **y0** : the origin of y
+                * **dy** : the derivative y pos
+                * **n** : ???
+                * **amp_noise** : the noise amplitude
+
+            Returns
+            -------
+                The computed data mock.
+        """
         x_axis = np.linspace(0, self.settings.child('Nx').value(), self.settings.child('Nx').value(),
                              endpoint=False)
         y_axis = np.linspace(0, self.settings.child('Ny').value(), self.settings.child('Ny').value(),
                              endpoint=False)
-        data_mock = self.settings['amp'] * (
-            mylib.gauss2D(x_axis, self.settings['x0'], self.settings['dx'],
-                          y_axis, self.settings['y0'], self.settings['dy'],
-                          self.settings['n'],
-                          angle=self.settings['angle']))\
-                    + self.settings['amp_noise'] * \
+        data_mock = self.settings.child('Amp').value() * (
+            mutils.gauss2D(x_axis, self.settings.child('x0').value(), self.settings.child('dx').value(),
+                          y_axis, self.settings.child('y0').value(), self.settings.child('dy').value(),
+                          self.settings.child('n').value())) + self.settings.child('amp_noise').value() * \
                     np.random.rand(len(y_axis), len(x_axis))
 
         for indy in range(data_mock.shape[0]):
             data_mock[indy, :] = data_mock[indy, :] * np.sin(x_axis / 4) ** 2
+        data_mock = np.roll(data_mock, self.ind_data * self.settings.child('rolling').value(), axis=1)
 
         if self.settings['use_roi_select']:
             x_axis, y_axis, data = \
@@ -100,33 +122,25 @@ class DAQ_2DViewer_Mock(DAQ_Viewer_base):
         else:
 
             self.image = data_mock
-        return self.image
 
-    def set_Mock_data(self):
-        self.image = np.roll(self.image, self.settings.child('rolling').value(), axis=1)
+        self.ind_data += 1
+
+        QThread.msleep(100)
+
         return self.image
 
     def ini_detector(self, controller=None):
-        """
-            Initialisation procedure of the detector initializing the status dictionnary.
+        self.ini_detector_init(controller, "Mock controller")
 
-            See Also
-            --------
-            daq_utils.ThreadCommand, get_xaxis, get_yaxis
-        """
-        self.ini_detector_init(controller, 'Mock controller')
-        self.emit_status(ThreadCommand('update_main_settings',
-                                       [['wait_time'], self.settings['wait_time'], 'value']))
-
-        self.x_axis = Axis(data=self.get_xaxis())
-        self.y_axis = Axis(data=self.get_yaxis())
-        self.base_Mock_data()
+        self.x_axis = self.get_xaxis()
+        self.y_axis = self.get_yaxis()
 
         # initialize viewers with the future type of data but with 0value data
-        self.data_grabed_signal_temp.emit(self.average_data(1, True),)
+        self.data_grabed_signal_temp.emit(self.average_data(1, True))
+        # OrderedDict(name='Mock3', data=[np.zeros((128,))], type='Data1D')])
 
         initialized = True
-        info = 'Controller ok'
+        info = 'Init'
         return info, initialized
 
     def close(self):
@@ -136,35 +150,11 @@ class DAQ_2DViewer_Mock(DAQ_Viewer_base):
         pass
 
     def get_xaxis(self):
-        """
-        Get the current x_axis from the Mock data setting.
-
-        Returns
-        -------
-        1D numpy array
-            the current x_axis.
-
-        See Also
-        --------
-        set_Mock_data
-        """
-        self.base_Mock_data()
+        self.set_Mock_data()
         return self.x_axis
 
     def get_yaxis(self):
-        """
-            Get the current y_axis from the Mock data setting.
-
-            Returns
-            -------
-            1D numpy array
-                the current y_axis.
-
-            See Also
-            --------
-            set_Mock_data
-        """
-        self.base_Mock_data()
+        self.set_Mock_data()
         return self.y_axis
 
     def grab_data(self, Naverage=1, **kwargs):
@@ -207,14 +197,12 @@ class DAQ_2DViewer_Mock(DAQ_Viewer_base):
             data_tmp += self.set_Mock_data()
         data_tmp = data_tmp / Naverage
 
-        data_tmp = data_tmp * (data_tmp >= self.settings.child('threshold').value()) * (init is False)
-        for ind in range(self.settings.child(('Nimagespannel')).value()):
+        data_tmp = data_tmp * (data_tmp >= self.settings['threshold']) * (init is False)
+        for ind in range(self.settings['Nimagespannel']):
             datatmptmp = []
-            for indbis in range(self.settings.child(('Nimagescolor')).value()):
+            for indbis in range(self.settings['Nimagescolor']):
                 datatmptmp.append(data_tmp)
-            data.append(DataFromPlugins(name='Mock2D_{:d}'.format(ind), data=datatmptmp, dim='Data2D',
-                                        x_axis=self.x_axis,
-                                        y_axis=self.y_axis))
+            data.append(DataFromPlugins(name='Mock2D_{:d}'.format(ind), data=datatmptmp, dim='Data2D'))
         # data.append(OrderedDict(name='Mock2D_1D',data=[np.mean(data_tmp,axis=0)], type='Data1D'))
         return data
 
