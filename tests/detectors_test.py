42a8b33e2f56a0e7444f448a2e5ad74efb39606d
Sébastien Weber <seba.weber@gmail.com>
2023-07-14 17:09:53 +0200

    setup tests with creation of mock detectors

---
+++ tests/detectors_test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 14/07/2023
+
+@author: Sebastien Weber
+"""
+
+import pytest
+from pymodaq.control_modules.utils import DET_TYPES, get_viewer_plugins, DAQTypesEnum, DetectorError
+
+from pymodaq.utils.daq_utils import get_plugins
+
+DET_TYPES = {'DAQ0D': get_plugins('daq_0Dviewer'),
+             'DAQ1D': get_plugins('daq_1Dviewer'),
+             'DAQ2D': get_plugins('daq_2Dviewer'),
+             'DAQND': get_plugins('daq_NDviewer'),
+             }
+
+
+def test_mock_detectors():
+    assert 'Mock' in [det['name'] for det in DET_TYPES['DAQ0D']]
+    assert 'Mock' in [det['name'] for det in DET_TYPES['DAQ1D']]
+    assert 'Mock' in [det['name'] for det in DET_TYPES['DAQ2D']]
+    assert 'Mock' in [det['name'] for det in DET_TYPES['DAQND']]
